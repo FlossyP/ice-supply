5964c02ad39eb54d57024be1f05c39cbc9798f5a
FlossyP <flossy0811@gmail.com>
2023-02-16 20:29:51 +0100

    123

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,39 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { MantineProvider } from '@mantine/core';
+import { extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  
+  const theme = extendTheme({
+    styles: {
+      global: () => ({
+        body: {
+          bg: "#121212",
+        },
+      }),
+    },
+  });
+
+  return (
+    <MantineProvider
+    withGlobalStyles
+    withNormalizeCSS
+    theme={{
+      colorScheme: 'dark',
+      colors: {
+        'purple-gray': ['#252028'],
+      },
+    }}
+    >
+      <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+      </ChakraProvider>
+      </MantineProvider>
+  )
+
 }
